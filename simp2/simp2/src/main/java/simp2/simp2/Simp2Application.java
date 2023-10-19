2da008bbbed1a06e3cd5bd2d21f36c74f1355794
Mads Kristensen <madsarke@gmail.com>
2023-10-19 18:36:21 +0200

    spring boot project

---
+++ simp2/simp2/src/main/java/simp2/simp2/Simp2Application.java
@@ -0,0 +1,13 @@
+package simp2.simp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simp2Application.class, args);
+	}
+
+}
---
+++ simp2/simp2/src/test/java/simp2/simp2/Simp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package simp2.simp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
